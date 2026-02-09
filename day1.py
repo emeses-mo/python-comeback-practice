6e1db7358cabd921187391c81eadf95ad4a4dc62
somemotime <reach.mohammedsameer@gmail.com>
2026-02-09 20:59:27 +0000

    Day1

---
+++ day1.py
@@ -0,0 +1,17 @@
+name = input("Enter Name: ")
+birthyear = int(input("Enter your birth year: "))
+
+curYr = 2026
+age =  curYr - birthyear
+
+print(f"Hello {name} - you are {age} yrs old.")
+
+yrsto30 = 30-age
+
+print(f" you will be 30 in {yrsto30} years")
+
+if age<18:
+    print("Minor")
+else:
+    print("Adult")
+    
